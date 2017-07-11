546af04b73480351cf618ebb7f8d5b18e5be0cec
Nick Smirnoff <Nick.Smirnoff@live.co.uk>
2017-07-11 21:17:19 +0100

    Connect with Spotify API. Previous/Skip Tracks & get currently playing.

--- Smallify/Form1.Designer.cs
+++ Smallify/Form1.Designer.cs
@@ -28,12 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSkip = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSkip
+            // 
+            this.btnSkip.Location = new System.Drawing.Point(188, 98);
+            this.btnSkip.Name = "btnSkip";
+            this.btnSkip.Size = new System.Drawing.Size(75, 23);
+            this.btnSkip.TabIndex = 0;
+            this.btnSkip.Text = "Skip";
+            this.btnSkip.UseVisualStyleBackColor = true;
+            this.btnSkip.Click += new System.EventHandler(this.btnSkip_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(12, 98);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(75, 23);
+            this.btnPrevious.TabIndex = 0;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(103, 193);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(949, 261);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnSkip);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSkip;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnUpdate;
     }
 }
 
--- Smallify/Form1.cs
+++ Smallify/Form1.cs
@@ -7,14 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SpotifyAPI.Local;
 
 namespace Smallify
 {
     public partial class Form1 : Form
     {
+        SpotifyLocalAPI _spotify = new SpotifyLocalAPI();
+
         public Form1()
         {
             InitializeComponent();
+            _spotify.Connect();
+        }
+
+        private void btnSkip_Click(object sender, EventArgs e)
+        {
+            _spotify.Skip();
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            _spotify.Previous();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            var model = _spotify.GetStatus();
+
+            label1.Text = model.Track.TrackResource.Name + " - " + model.Track.ArtistResource.Name;
         }
     }
 }
