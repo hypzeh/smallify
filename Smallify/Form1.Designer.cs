f2346d738936b46adf28ee094a8788661dea6133
Nick Smirnoff <Nick.Smirnoff@live.co.uk>
2017-07-11 21:44:34 +0100

    Previous/Skip Buttons with UI.

--- Smallify/Form1.Designer.cs
+++ Smallify/Form1.Designer.cs
@@ -28,31 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnSkip = new System.Windows.Forms.Button();
-            this.btnPrevious = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.btnUpdate = new System.Windows.Forms.Button();
+            this.pbtnSkip = new System.Windows.Forms.PictureBox();
+            this.pbtnPrevious = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbtnSkip)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbtnPrevious)).BeginInit();
             this.SuspendLayout();
-            // 
-            // btnSkip
-            // 
-            this.btnSkip.Location = new System.Drawing.Point(188, 98);
-            this.btnSkip.Name = "btnSkip";
-            this.btnSkip.Size = new System.Drawing.Size(75, 23);
-            this.btnSkip.TabIndex = 0;
-            this.btnSkip.Text = "Skip";
-            this.btnSkip.UseVisualStyleBackColor = true;
-            this.btnSkip.Click += new System.EventHandler(this.btnSkip_Click);
-            // 
-            // btnPrevious
-            // 
-            this.btnPrevious.Location = new System.Drawing.Point(12, 98);
-            this.btnPrevious.Name = "btnPrevious";
-            this.btnPrevious.Size = new System.Drawing.Size(75, 23);
-            this.btnPrevious.TabIndex = 0;
-            this.btnPrevious.Text = "Previous";
-            this.btnPrevious.UseVisualStyleBackColor = true;
-            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
             // 
             // label1
             // 
@@ -65,7 +47,7 @@
             // 
             // btnUpdate
             // 
-            this.btnUpdate.Location = new System.Drawing.Point(103, 193);
+            this.btnUpdate.Location = new System.Drawing.Point(418, 206);
             this.btnUpdate.Name = "btnUpdate";
             this.btnUpdate.Size = new System.Drawing.Size(75, 23);
             this.btnUpdate.TabIndex = 2;
@@ -73,28 +55,54 @@
             this.btnUpdate.UseVisualStyleBackColor = true;
             this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
+            // pbtnSkip
+            // 
+            this.pbtnSkip.Image = global::Smallify.Properties.Resources.skip_default;
+            this.pbtnSkip.Location = new System.Drawing.Point(648, 82);
+            this.pbtnSkip.Name = "pbtnSkip";
+            this.pbtnSkip.Size = new System.Drawing.Size(32, 32);
+            this.pbtnSkip.TabIndex = 3;
+            this.pbtnSkip.TabStop = false;
+            this.pbtnSkip.Click += new System.EventHandler(this.pbtnSkip_Click);
+            this.pbtnSkip.MouseLeave += new System.EventHandler(this.pbtnSkip_MouseLeave);
+            this.pbtnSkip.MouseHover += new System.EventHandler(this.pbtnSkip_MouseHover);
+            // 
+            // pbtnPrevious
+            // 
+            this.pbtnPrevious.Image = global::Smallify.Properties.Resources.previous_default;
+            this.pbtnPrevious.Location = new System.Drawing.Point(512, 82);
+            this.pbtnPrevious.Name = "pbtnPrevious";
+            this.pbtnPrevious.Size = new System.Drawing.Size(32, 32);
+            this.pbtnPrevious.TabIndex = 3;
+            this.pbtnPrevious.TabStop = false;
+            this.pbtnPrevious.Click += new System.EventHandler(this.pbtnPrevious_Click);
+            this.pbtnPrevious.MouseLeave += new System.EventHandler(this.pbtnPrevious_MouseLeave);
+            this.pbtnPrevious.MouseHover += new System.EventHandler(this.pbtnPrevious_MouseHover);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
             this.ClientSize = new System.Drawing.Size(949, 261);
+            this.Controls.Add(this.pbtnPrevious);
+            this.Controls.Add(this.pbtnSkip);
             this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnPrevious);
-            this.Controls.Add(this.btnSkip);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbtnSkip)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbtnPrevious)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnSkip;
-        private System.Windows.Forms.Button btnPrevious;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.PictureBox pbtnSkip;
+        private System.Windows.Forms.PictureBox pbtnPrevious;
     }
 }
 
--- Smallify/Form1.cs
+++ Smallify/Form1.cs
@@ -21,21 +21,41 @@ namespace Smallify
             _spotify.Connect();
         }
 
-        private void btnSkip_Click(object sender, EventArgs e)
-        {
-            _spotify.Skip();
-        }
-
-        private void btnPrevious_Click(object sender, EventArgs e)
-        {
-            _spotify.Previous();
-        }
-
         private void btnUpdate_Click(object sender, EventArgs e)
         {
             var model = _spotify.GetStatus();
 
             label1.Text = model.Track.TrackResource.Name + " - " + model.Track.ArtistResource.Name;
+        }
+
+        private void pbtnSkip_MouseHover(object sender, EventArgs e)
+        {
+            pbtnSkip.Image = Properties.Resources.skip_hover;
+        }
+
+        private void pbtnSkip_MouseLeave(object sender, EventArgs e)
+        {
+            pbtnSkip.Image = Properties.Resources.skip_default;
+        }
+
+        private void pbtnSkip_Click(object sender, EventArgs e)
+        {
+            _spotify.Skip();
+        }
+   
+        private void pbtnPrevious_MouseHover(object sender, EventArgs e)
+        {
+            pbtnPrevious.Image = Properties.Resources.previous_hover;
+        }
+
+        private void pbtnPrevious_MouseLeave(object sender, EventArgs e)
+        {
+            pbtnPrevious.Image = Properties.Resources.previous_default;
+        }
+
+        private void pbtnPrevious_Click(object sender, EventArgs e)
+        {
+            _spotify.Previous();
         }
     }
 }
