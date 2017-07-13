def3c428b4ee69d30756e34e2dd00da2c60de059
Nick Smirnoff <Nick.Smirnoff@live.co.uk>
2017-07-13 21:05:42 +0100

    Squirrel.Windows integration.

--- Smallify/Program.cs
+++ Smallify/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Squirrel;
 
 namespace Smallify
 {
@@ -14,6 +15,16 @@ namespace Smallify
         [STAThread]
         static void Main()
         {
+
+            // Squirrel --releasify smallify.x.x.x.nupkg --no-msi
+            Task.Run(async () =>
+            {
+                using (var mgr = new UpdateManager("http://nicksmirnoff.co.uk/projects/smallify/install/"))
+                {
+                    await mgr.UpdateApp();
+                }
+            });
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
