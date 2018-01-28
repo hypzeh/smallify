7091fcf31f5d4ca19919e8e66314b23a02759894
Nick Smirnoff <nick.smirnoff@live.co.uk>
2018-01-28 12:56:34 +0000

    Added Context Menu style

--- GUI/ViewModels/ShellViewModel.cs
+++ GUI/ViewModels/ShellViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using GUI.Shared.Constants;
+using Player.Models;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Regions;
+using System.ComponentModel;
 using System.Windows.Input;
 
 namespace GUI.ViewModels
@@ -9,13 +11,18 @@ namespace GUI.ViewModels
 	public class ShellViewModel : BindableBase
 	{
 		private readonly IRegionManager _regionManager;
+		private readonly PlayerModel _player;
+
 		private string _title;
 
-		public ShellViewModel(IRegionManager regionManager)
+		public ShellViewModel(IRegionManager regionManager, PlayerModel player)
 		{
 			this._regionManager = regionManager;
+			this._player = player;
 
-			this._title = "Smallify";
+			this._title = $"{this._player?.CurrentTrack?.TrackResource?.Name ?? "Smallify"} - {this._player?.CurrentTrack?.ArtistResource?.Name ?? string.Empty}";
+
+			this._player.PropertyChanged += this.Player_PropertyChanged;
 
 			this.SwitchPlayerCommand = new DelegateCommand<string>(this.SwitchPlayerCommand_Execute);
 		}
@@ -38,6 +45,19 @@ namespace GUI.ViewModels
 		public void SwitchPlayerCommand_Execute(string playerRequest)
 		{
 			this._regionManager.RequestNavigate(RegionNames.PlayerRegion, playerRequest);
+		}
+
+		private void Player_PropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			switch (e.PropertyName)
+			{
+				case nameof(this._player.CurrentTrack):
+					var status = this._player.GetClientStatus();
+
+					this.Title = $"{status?.Track?.TrackResource?.Name ?? "Smallify"} - {status?.Track?.ArtistResource?.Name ?? string.Empty}";
+					
+					break;
+			}
 		}
 	}
 }
