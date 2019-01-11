87f18a2ac671e7457df4e54bdbb4c61f0afb4e8f
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-11 22:36:14 +0000

    Added Prism and renamed MainWindow to Shell

---
+++ src/Smallify.GUI/Shell.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Smallify.GUI
+{
+    public partial class Shell : Window
+    {
+        public Shell()
+        {
+            InitializeComponent();
+        }
+    }
+}
