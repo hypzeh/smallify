924b02be5d41619cffe376f29455f8985af6fcdf
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-12 13:41:30 +0000

    Added properties and bindings for track details

--- src/Smallify.Module.Player/ViewModels/PlayerViewModel.cs
+++ src/Smallify.Module.Player/ViewModels/PlayerViewModel.cs
@@ -7,9 +7,16 @@ namespace Smallify.Module.Player.ViewModels
 {
 	public class PlayerViewModel : BindableBase
 	{
+		private string _trackName;
+		private string _trackArtist;
+		private string _trackAlbumName;
 
 		public PlayerViewModel()
 		{
+			_trackName = string.Empty;
+			_trackArtist = string.Empty;
+			_trackAlbumName = string.Empty;
+
 			PreviousCommand = new DelegateCommand(PreviousCommand_Execute);
 			PlayCommand = new DelegateCommand(PlayCommand_Execute);
 			PauseCommand = new DelegateCommand(PauseCommand_Execute);
@@ -23,6 +30,24 @@ namespace Smallify.Module.Player.ViewModels
 		public ICommand PauseCommand { get; private set; }
 
 		public ICommand SkipCommand { get; private set; }
+
+		public string TrackName
+		{
+			get => _trackName;
+			set => SetProperty(ref _trackName, value);
+		}
+
+		public string TrackArtist
+		{
+			get => _trackArtist;
+			set => SetProperty(ref _trackArtist, value);
+		}
+
+		public string TrackAlbumName
+		{
+			get => _trackAlbumName;
+			set => SetProperty(ref _trackAlbumName, value);
+		}
 
 		private void PreviousCommand_Execute()
 		{
