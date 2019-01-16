656d2bd21baebc3c424e9db12f6664c9108f89d3
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-16 21:32:16 +0000

    Added dummy notification list

--- src/Smallify.Module.Notifications/ViewModels/NotificationsShellViewModel.cs
+++ src/Smallify.Module.Notifications/ViewModels/NotificationsShellViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Input;
 
@@ -10,8 +11,20 @@ namespace Smallify.Module.Notifications.ViewModels
 		public NotificationsShellViewModel()
 		{
 			ExitCommand = new DelegateCommand<Window>((window) => window.Close());
+			AddNotificationCommand = new DelegateCommand(() => Notifications.Add("some notification..."));
+
+			Notifications = new ObservableCollection<string>()
+			{
+				"123",
+				"456",
+				"789"
+			};
 		}
 
 		public ICommand ExitCommand { get; private set; }
+
+		public ICommand AddNotificationCommand { get; private set; }
+
+		public ObservableCollection<string> Notifications { get; set; }
 	}
 }
