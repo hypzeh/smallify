932a9110b55104028c2652a2b3b4b10b52c4df4c
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-16 20:04:30 +0000

    Added notifications shell view model

---
+++ src/Smallify.Module.Notifications/ViewModels/NotificationsShellViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Smallify.Module.Notifications.ViewModels
+{
+	public class NotificationsShellViewModel : BindableBase
+	{
+		public NotificationsShellViewModel()
+		{
+			ExitCommand = new DelegateCommand<Window>((window) => window.Close());
+		}
+
+		public ICommand ExitCommand { get; private set; }
+	}
+}
