0421fb5f9f3e2dae0b652ab13d6ec2ae9d7277e0
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-19 20:17:53 +0000

    Created spotify api service

---
+++ src/Smallify.Module.Player/Services/SpotifyAPI.cs
@@ -0,0 +1,33 @@
+﻿using Smallify.Module.Core;
+using SpotifyAPI.Web;
+using System.ComponentModel;
+
+namespace Smallify.Module.Player.Services
+{
+	public class SpotifyAPI
+	{
+		private readonly IConfiguration _configuration;
+		private readonly SpotifyWebAPI _spotify;
+
+		public SpotifyAPI(IConfiguration configuration)
+		{
+			_configuration = configuration;
+			_spotify = new SpotifyWebAPI
+			{
+				UseAuth = true,
+				TokenType = "Bearer",
+				AccessToken = configuration.AccessToken
+			};
+
+			_configuration.PropertyChanged += Configuration_PropertyChanged;
+		}
+
+		private void Configuration_PropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			if (e.PropertyName == nameof(IConfiguration.AccessToken))
+			{
+				_spotify.AccessToken = _configuration.AccessToken;
+			}
+		}
+	}
+}
