1365b0144ab4bf4845369df5183124496fabf9b9
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-20 21:42:23 +0000

    Created settings view model with save command

---
+++ src/Smallify.Module.Settings/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Windows.Input;
+
+namespace Smallify.Module.Settings.ViewModels
+{
+	public class SettingsViewModel : BindableBase
+	{
+		public SettingsViewModel()
+		{
+			SaveCommand = new DelegateCommand(SaveCommand_Execute);
+		}
+
+		public ICommand SaveCommand { get; }
+
+		private void SaveCommand_Execute()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
