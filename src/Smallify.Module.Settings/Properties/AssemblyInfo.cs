a12b26b244ec2cf76bc07a7c7e52161131e4831f
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-20 21:02:53 +0000

    Created settings module

---
+++ src/Smallify.Module.Settings/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Smallify.Module.Settings")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Smallify.Module.Settings")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("167a71c7-f26f-4b84-8be8-78149be03fa4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Smallify.Module.Settings/SettingsModule.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+
+namespace Smallify.Module.Settings
+{
+	public class SettingsModule : IModule
+	{
+		public void OnInitialized(IContainerProvider containerProvider)
+		{
+
+		}
+
+		public void RegisterTypes(IContainerRegistry containerRegistry)
+		{
+
+		}
+	}
+}
