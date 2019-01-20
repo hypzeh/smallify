3e2e8233c3ef7deda294a00b7af5d06060bfae8c
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-20 21:22:57 +0000

    Added settings button view model with show settings window command

---
+++ src/Smallify.Module.Settings/ViewModels/SettingsButtonViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Windows.Input;
+
+namespace Smallify.Module.Settings.ViewModels
+{
+	public class SettingsButtonViewModel : BindableBase
+	{
+		public SettingsButtonViewModel()
+		{
+			ShowSettingsWindowCommand = new DelegateCommand(ShowSettingsWindowCommand_Execute);
+		}
+
+		public ICommand ShowSettingsWindowCommand { get; }
+
+		private void ShowSettingsWindowCommand_Execute()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
