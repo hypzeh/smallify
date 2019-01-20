1c07d063c68c393ec1f0c5ba9988eff63400ea89
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-20 21:37:17 +0000

    Created settings view

---
+++ src/Smallify.Module.Settings/Views/SettingsView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Smallify.Module.Settings.Views
+{
+	public partial class SettingsView : UserControl
+	{
+		public SettingsView()
+		{
+			InitializeComponent();
+		}
+	}
+}
