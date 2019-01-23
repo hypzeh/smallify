9f1f1d5ad68e5e6d146ba796b44f9773925a2502
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-23 21:38:34 +0000

    Added wwwroot

--- src/Smallify.Web/Startup.cs
+++ src/Smallify.Web/Startup.cs
@@ -22,7 +22,6 @@ namespace Smallify.Web
 			});
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
 			app.UseHsts();
