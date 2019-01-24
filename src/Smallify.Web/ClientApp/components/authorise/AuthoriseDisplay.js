eec831bd02b264c11502feeece3e8480c3901b51
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-24 22:22:02 +0000

    Added authorise on click function

--- src/Smallify.Web/ClientApp/components/authorise/AuthoriseDisplay.js
+++ src/Smallify.Web/ClientApp/components/authorise/AuthoriseDisplay.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+const AuthoriseSpotify = () => {
+    const clientID = 'c3e67d4f6ab143ddb876938aece626af';
+    const redirectURI = 'http:%2F%2Flocalhost:5250%2Fcallback';
+    const scopes = 'user-modify-playback-state%20user-read-playback-state%20user-read-currently-playing';
+    const authoriseURL = `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&scope=${scopes}&response_type=token`;
+
+    window.location = authoriseURL;
+}
+
 const AuthroiseDisplay = () => (
-    <button>
+    <button onClick={AuthoriseSpotify}>
         Connect
     </button>
 );
