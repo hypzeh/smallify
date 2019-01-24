6796780941dd4a9e3d13fad59c60c425b94940fc
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-24 21:36:22 +0000

    Created authorise callback component

---
+++ src/Smallify.Web/ClientApp/components/authorise/AuthoriseCallback.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AuthoriseCallback = ({ location }) => {
+  console.log(location);
+  return (
+    <label>
+      Access Token:
+      <input 
+      type="text"
+      value={location.search}/>
+    </label>
+  );
+};
+
+export default AuthoriseCallback;
