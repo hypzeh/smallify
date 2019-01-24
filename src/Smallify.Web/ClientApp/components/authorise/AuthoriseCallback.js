2ac60425df6e71fa1c3ed87560ba92ddbd40578a
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-24 23:00:22 +0000

    Added copy access token function

--- src/Smallify.Web/ClientApp/components/authorise/AuthoriseCallback.js
+++ src/Smallify.Web/ClientApp/components/authorise/AuthoriseCallback.js
@@ -1,13 +1,22 @@
 import React from "react";
-import queryString from 'query-string';
+import queryString from "query-string";
+
+const CopyAccessToken = () => {
+  var copyText = document.getElementById("accessToken");
+  copyText.select();
+  document.execCommand("copy");
+};
 
 const AuthoriseCallback = ({ location }) => {
   const { access_token } = queryString.parse(location.hash);
   return (
-    <label>
-      Access Token:
-      <input type="text" value={access_token} />
-    </label>
+    <React.Fragment>
+      <label>
+        Access Token:
+        <input id="accessToken" type="text" value={access_token} readOnly />
+      </label>
+      <button onClick={CopyAccessToken}>Copy</button>
+    </React.Fragment>
   );
 };
 
