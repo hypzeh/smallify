c9553f61b2a0c2bc0cbea9514ce1b4e12439ac95
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-24 19:45:15 +0000

    Experiment/project pages (#10)
    * Created smallify web project
    
    * Updated package-lock
    
    * Removed general boilerplate from startup
    
    * Removed sample data boilerplate
    
    * Added wwwroot
    
    * Removed pages
    
    * Removed boilerplate and setup project structure
    
    * Added gh-pages package to dev dependencies
    
    * Updated packages
    
    * Added deploy gh-pages script
    
    * Attempt to only build smallify gui
    
    * Removed smallify web project from solution msbuild

---
+++ src/Smallify.Web/Startup.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Smallify.Web
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddSpaStaticFiles(configuration =>
+			{
+				configuration.RootPath = "wwwroot";
+			});
+		}
+
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			app.UseHsts();
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+			app.UseSpaStaticFiles();
+			app.UseSpa(spa =>
+			{
+				spa.Options.SourcePath = "ClientApp";
+			});
+		}
+	}
+}
