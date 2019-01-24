ff8af97c6a01a54813b8a715e34f569012308bd2
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-24 21:36:45 +0000

    Updated route to callback

--- src/Smallify.Web/ClientApp/routers/AppRouter.js
+++ src/Smallify.Web/ClientApp/routers/AppRouter.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import AuthoriseDisplay from '../components/authorise/AuthoriseDisplay';
+import AuthoriseCallback from '../components/authorise/AuthoriseCallback';
 import NotFound from '../components/shared/NotFound';
 
 const AppRouter = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={AuthoriseDisplay} />
+      <Route exact path="/callback" component={AuthoriseCallback} />
       <Route path="*" component={NotFound} status={404} />
     </Switch>
   </BrowserRouter>
