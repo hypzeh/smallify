0d89b5ca0e4ea66ec3a8da3735af0a9fb82aa3b4
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-24 20:25:24 +0000

    Updated app router to route 404

--- src/Smallify.Web/ClientApp/routers/AppRouter.js
+++ src/Smallify.Web/ClientApp/routers/AppRouter.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import NotFound from '../components/shared/NotFound';
+
 const AppRouter = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={() => <p>Test</p>} />
+      <Route path="*" component={NotFound} status={404} />
     </Switch>
   </BrowserRouter>
 );
