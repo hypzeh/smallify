395349c4c04f3e1aea701cec39a8e0051ee870ba
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-27 21:28:57 +0000

    Created about view for settings

---
+++ src/Smallify.Module.Settings/Views/Sections/About.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Smallify.Module.Settings.Views.Sections
+{
+	public partial class About : UserControl
+	{
+		public About()
+		{
+			InitializeComponent();
+		}
+	}
+}
