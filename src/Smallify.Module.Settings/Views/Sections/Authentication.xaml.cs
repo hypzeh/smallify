0d73441d12c6bfede6e32c27ad3f8610245a8283
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-28 21:30:59 +0000

    Added bindings to authentication view model

--- src/Smallify.Module.Settings/Views/Sections/Authentication.xaml.cs
+++ src/Smallify.Module.Settings/Views/Sections/Authentication.xaml.cs
@@ -1,12 +1,14 @@
-﻿using System.Windows.Controls;
+﻿using Smallify.Module.Settings.ViewModels;
+using System.Windows.Controls;
 
 namespace Smallify.Module.Settings.Views.Sections
 {
 	public partial class Authentication : UserControl
 	{
-		public Authentication()
+		public Authentication(IAuthenticationViewModel authenticationViewModel)
 		{
 			InitializeComponent();
+			DataContext = authenticationViewModel;
 		}
 	}
 }
