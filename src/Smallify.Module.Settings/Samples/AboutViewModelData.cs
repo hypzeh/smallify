a737e58a02885cb88d90a783cb948e9178a1b1b9
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-28 22:11:18 +0000

    Created about view design time sample data

---
+++ src/Smallify.Module.Settings/Samples/AboutViewModelData.cs
@@ -0,0 +1,12 @@
+﻿using Smallify.Module.Settings.ViewModels;
+using System.Windows.Input;
+
+namespace Smallify.Module.Settings.Samples
+{
+	public class AboutViewModelData : IAboutViewModel
+	{
+		public ICommand OpenGithubCommand { get; }
+
+		public string Version => "v0.0.0-alpha";
+	}
+}
