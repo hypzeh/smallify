4ccaa43bf0ef201e147dcb49b3c23bbbf5e7d1d2
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-28 22:17:23 +0000

    Added bindings to about view

--- src/Smallify.Module.Settings/Views/Sections/About.xaml.cs
+++ src/Smallify.Module.Settings/Views/Sections/About.xaml.cs
@@ -1,12 +1,14 @@
-﻿using System.Windows.Controls;
+﻿using Smallify.Module.Settings.ViewModels;
+using System.Windows.Controls;
 
 namespace Smallify.Module.Settings.Views.Sections
 {
 	public partial class About : UserControl
 	{
-		public About()
+		public About(IAboutViewModel aboutViewModel)
 		{
 			InitializeComponent();
+			DataContext = aboutViewModel;
 		}
 	}
 }
