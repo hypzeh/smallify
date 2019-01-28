91708cc507a3f21314578b18b9b404ccc3f6a2ea
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-28 21:18:24 +0000

    Added access token property to authentication view model

--- src/Smallify.Module.Settings/ViewModels/AuthenticationViewModel.cs
+++ src/Smallify.Module.Settings/ViewModels/AuthenticationViewModel.cs
@@ -4,6 +4,17 @@ namespace Smallify.Module.Settings.ViewModels
 {
 	public class AuthenticationViewModel : BindableBase
 	{
+		private string _accessToken;
 
+		public AuthenticationViewModel()
+		{
+			_accessToken = string.Empty;
+		}
+
+		public string AccessToken
+		{
+			get => _accessToken;
+			set => SetProperty(ref _accessToken, value);
+		}
 	}
 }
