3434d13ed2a8195371e6434aa460e5ff26432c8a
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-28 21:20:29 +0000

    Added get access token command

--- src/Smallify.Module.Settings/ViewModels/AuthenticationViewModel.cs
+++ src/Smallify.Module.Settings/ViewModels/AuthenticationViewModel.cs
@@ -1,5 +1,8 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 using Smallify.Module.Core;
+using System;
+using System.Windows.Input;
 
 namespace Smallify.Module.Settings.ViewModels
 {
@@ -14,12 +17,21 @@ namespace Smallify.Module.Settings.ViewModels
 			_configuration = configuration;
 
 			_accessToken = configuration.AccessToken;
+
+			GetAccessTokenCommand = new DelegateCommand(GetAccessTokenCommand_Execute);
 		}
+
+		public ICommand GetAccessTokenCommand { get; }
 
 		public string AccessToken
 		{
 			get => _accessToken;
 			set => SetProperty(ref _accessToken, value);
+		}
+
+		private void GetAccessTokenCommand_Execute()
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
