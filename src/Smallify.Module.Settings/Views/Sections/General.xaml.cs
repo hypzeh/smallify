23452f34657bdbda43a50a93f43a6bf76cfbce98
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-29 19:16:13 +0000

    Created general settings view

---
+++ src/Smallify.Module.Settings/Views/Sections/General.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Smallify.Module.Settings.Views.Sections
+{
+	public partial class General : UserControl
+	{
+		public General()
+		{
+			InitializeComponent();
+		}
+	}
+}
