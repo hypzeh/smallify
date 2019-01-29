e23a0a7131bfeeba3b166c319ff92e25be20f29f
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-29 21:59:44 +0000

    Added configuration to gui shell view model

--- src/Smallify.GUI/ViewModels/ShellViewModel.cs
+++ src/Smallify.GUI/ViewModels/ShellViewModel.cs
@@ -1,20 +1,32 @@
 ﻿using Prism.Commands;
+using Prism.Events;
 using Prism.Mvvm;
+using Smallify.Module.Core;
+using Smallify.Module.Core.Events.Configuration;
 using System.Windows;
 using System.Windows.Input;
 
 namespace Smallify.GUI.ViewModels
 {
-	public class ShellViewModel : BindableBase
+	public class ShellViewModel : BindableBase, IShellViewModel
 	{
-		public ShellViewModel()
+		public ShellViewModel(
+			IEventAggregator eventAggregator,
+			IConfiguration configuration)
 		{
+			Configuration = configuration;
+
 			ExitCommand = new DelegateCommand(() => Application.Current.Shutdown());
 			MinimiseCommand = new DelegateCommand<Window>(window => window.WindowState = WindowState.Minimized);
+
+			eventAggregator.GetEvent<OnConfigurationChangedEvent>()
+				?.Subscribe(args => RaisePropertyChanged(nameof(Configuration)));
 		}
 
-		public ICommand ExitCommand { get; private set; }
+		public ICommand ExitCommand { get; }
 
-		public ICommand MinimiseCommand { get; private set; }
+		public ICommand MinimiseCommand { get; }
+
+		public IConfiguration Configuration { get; }
 	}
 }
