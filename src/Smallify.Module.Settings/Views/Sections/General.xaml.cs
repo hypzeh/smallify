274a0f18b3b88f08788ae1791310974057b91ea2
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-29 19:29:59 +0000

    Updated always on top checkbox to binding

--- src/Smallify.Module.Settings/Views/Sections/General.xaml.cs
+++ src/Smallify.Module.Settings/Views/Sections/General.xaml.cs
@@ -1,12 +1,14 @@
-﻿using System.Windows.Controls;
+﻿using Smallify.Module.Settings.ViewModels;
+using System.Windows.Controls;
 
 namespace Smallify.Module.Settings.Views.Sections
 {
 	public partial class General : UserControl
 	{
-		public General()
+		public General(IGeneralViewModel generalViewModel)
 		{
 			InitializeComponent();
+			DataContext = generalViewModel;
 		}
 	}
 }
