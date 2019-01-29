1fe86843a9d7702e5c79e4120489febf02993af0
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-29 19:26:13 +0000

    Created general view model

---
+++ src/Smallify.Module.Settings/ViewModels/GeneralViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Prism.Mvvm;
+using Smallify.Module.Core;
+
+namespace Smallify.Module.Settings.ViewModels
+{
+	public class GeneralViewModel : BindableBase
+	{
+		private readonly IConfiguration _configuration;
+
+		private bool _alwaysOnTopEnabled;
+
+		public GeneralViewModel(IConfiguration configuration)
+		{
+			_configuration = configuration;
+
+			_alwaysOnTopEnabled = configuration.AlwaysOnTop;
+		}
+
+		public bool AlwaysOnTopEnabled
+		{
+			get => _alwaysOnTopEnabled;
+			set
+			{
+				if (SetProperty(ref _alwaysOnTopEnabled, value))
+				{
+					_configuration.AlwaysOnTop = value;
+				}
+			}
+		}
+	}
+}
