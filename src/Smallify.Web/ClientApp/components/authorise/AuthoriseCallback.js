671a12f94a3f8b1cc878c6c43e200e5edde4994d
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-31 21:15:17 +0000

    Updated the callback component

--- src/Smallify.Web/ClientApp/components/authorise/AuthoriseCallback.js
+++ src/Smallify.Web/ClientApp/components/authorise/AuthoriseCallback.js
@@ -1,22 +1,27 @@
 import React from "react";
 import queryString from "query-string";
 
+import Container from "../shared/styled/container";
+import TextArea from "../shared/styled/textArea";
+import Button from "../shared/styled/button";
+import SmallifyIcon from "../shared/SmallifyIcon";
+
 const CopyAccessToken = () => {
-  var copyText = document.getElementById("accessToken");
-  copyText.select();
+  var token = document.getElementById("token");
+  token.select();
   document.execCommand("copy");
 };
 
 const AuthoriseCallback = ({ location }) => {
   const { access_token } = queryString.parse(location.hash);
   return (
-    <React.Fragment>
-      <label>
-        Access Token:
-        <input id="accessToken" type="text" value={access_token} readOnly />
-      </label>
-      <button onClick={CopyAccessToken}>Copy</button>
-    </React.Fragment>
+    <Container>
+      <SmallifyIcon />
+      <p>Copy the access token below and paste it in Smallify.</p>
+      <TextArea id="token" value={access_token} readOnly />
+      <br />
+      <Button onClick={CopyAccessToken}>COPY</Button>
+    </Container>
   );
 };
 
