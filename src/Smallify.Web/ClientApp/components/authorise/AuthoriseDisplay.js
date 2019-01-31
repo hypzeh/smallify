721507ce97c35e7b5a0a493099a85666f9548ccc
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-31 20:32:44 +0000

    Updated authorisation display component

--- src/Smallify.Web/ClientApp/components/authorise/AuthoriseDisplay.js
+++ src/Smallify.Web/ClientApp/components/authorise/AuthoriseDisplay.js
@@ -1,38 +1,25 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
 
-import { Authentication } from "../../lib/authentication";
-
-const Button = styled.button`
-  color: #fff;
-  background-color: #1aa34a;
-  font-size: 14px;
-  line-height: 1;
-  border-radius: 500px;
-  padding: 18px 48px 16px;
-  transition-property: background-color,border-color,color,box-shadow,filter;
-  transition-duration: .3s;
-  border-width: 0;
-  letter-spacing: 2px;
-  min-width: 160px;
-  text-transform: uppercase;
-  white-space: normal;
-  :hover {
-    background-color: #1db954;
-  }
-`;
+import { Authentication } from '../../lib/authentication';
+import Container from '../shared/styled/container';
+import Button from '../shared/styled/button';
+import SmallifyIcon from '../shared/SmallifyIcon';
 
 const AuthoriseSpotify = () => {
   const { ClientID, RedirectURI, Scopes } = Authentication;
-  window.location = `https://accounts.spotify.com/authorize
-    ?client_id=${encodeURIComponent(ClientID)}
-    &redirect_uri=${encodeURIComponent(RedirectURI)}
-    &scope=${encodeURIComponent(Scopes)}
-    &response_type=token`;
+  window.location = "https://accounts.spotify.com/authorize"
+    + `?client_id=${encodeURIComponent(ClientID)}`
+    + `&redirect_uri=${encodeURIComponent(RedirectURI)}`
+    + `&scope=${encodeURIComponent(Scopes)}`
+    + `&response_type=token`;
 };
 
 const AuthroiseDisplay = () => (
-  <Button onClick={AuthoriseSpotify}>CONNECT</Button>
+  <Container>
+    <SmallifyIcon />
+    <br />
+    <Button onClick={AuthoriseSpotify}>CONNECT</Button>
+  </Container>
 );
 
 export default AuthroiseDisplay;
