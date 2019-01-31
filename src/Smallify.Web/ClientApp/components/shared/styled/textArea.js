685c9c18b32c241333ef48d24017c02720769eff
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-01-31 21:14:27 +0000

    Created text area styled component

---
+++ src/Smallify.Web/ClientApp/components/shared/styled/textArea.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const TextArea = styled.textarea`
+  width: 100%;
+  max-height: 100px;
+  max-width: 500px;
+  min-height: 20px;
+  min-width: 300px;
+`;
+
+export default TextArea;
