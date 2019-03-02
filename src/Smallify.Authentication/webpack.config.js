2adecce8c1825abf4ef41a29b932999985c156be
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-03-02 23:07:02 +0000

    Added react webpack babel and eslint

---
+++ src/Smallify.Authentication/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './ClientApp/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'wwwroot'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './ClientApp/template.html',
+      filename: './index.html',
+    }),
+  ],
+};
