3bd104f4451eb1c23a9e990c59532379813218e0
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-03-02 22:42:17 +0000

    Setup basic project

--- src/Smallify.Authentication/Startup.cs
+++ src/Smallify.Authentication/Startup.cs
@@ -1,33 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Smallify.Authentication
 {
 	public class Startup
 	{
-		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddSpaStaticFiles(configuration =>
+			{
+				configuration.RootPath = "wwwroot";
+			});
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
-			if (env.IsDevelopment())
+			app.UseDefaultFiles();
+			app.UseSpaStaticFiles();
+			app.UseSpa(spa =>
 			{
-				app.UseDeveloperExceptionPage();
-			}
-
-			app.Run(async (context) =>
-			{
-				await context.Response.WriteAsync("Hello World!");
+				spa.Options.SourcePath = ".";
 			});
 		}
 	}
