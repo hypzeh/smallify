7b0753db065165b1c08ea955eca8380a6356f756
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-03-03 00:33:37 +0000

    Created global style

---
+++ src/Smallify.Authentication/ClientApp/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import GlobalStyle from './styles/globalStyle';
+
+const App = () => (
+  <React.Fragment>
+    <GlobalStyle/>
+    <h1>Hello World!</h1>
+  </React.Fragment>
+);
+
+export default App;
--- src/Smallify.Authentication/ClientApp/index.jsx
+++ src/Smallify.Authentication/ClientApp/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './App';
+
 ReactDOM.render(
-  <h1>Hello World!</h1>,
+  <App />,
   document.getElementById('root'),
 );
