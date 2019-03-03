28dcea8ace1ead02a0e56b29a719cff52ab6ee1c
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-03-03 19:07:56 +0000

    Created SmallifyContainer component with basic styling

---
+++ src/Smallify.Authentication/ClientApp/styles/components/shared/smallifyContainer.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const SmallifyContainer = {};
+
+SmallifyContainer.Container = styled.div`
+  text-align: center;
+`;
+
+SmallifyContainer.Content = styled.div`
+  display: block;
+`;
+
+export default SmallifyContainer;
