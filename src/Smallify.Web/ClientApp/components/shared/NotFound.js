31d181fc1d21de05cf428cff16f3f363b95efbf4
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-03-03 21:28:36 +0000

    Removed Smallify.Web project from master branch

--- src/Smallify.Web/ClientApp/components/shared/NotFound.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const NotFound = () => <h1>404 - Not Found</h1>;
-
-export default NotFound;
--- src/Smallify.Web/ClientApp/components/shared/styled/paragraph.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-const Paragraph = styled.p`
-  font-family: Helvetica, Arial, Sans-Serif;
-`;
-
-export default Paragraph;
