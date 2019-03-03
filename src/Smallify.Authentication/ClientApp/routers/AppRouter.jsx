7e21c05936c1ca89e19405533816fa57d541a3bc
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-03-03 19:24:49 +0000

    Added a route for AuthenticationCallback

--- src/Smallify.Authentication/ClientApp/routers/AppRouter.jsx
+++ src/Smallify.Authentication/ClientApp/routers/AppRouter.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import AuthenticationDisplay from '../components/authentication/AuthenticationDisplay';
+import AuthenticationCallback from '../components/authentication/AuthenticationCallback';
 
 const AppRouter = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={AuthenticationDisplay} />
+      <Route exact path="/callback" component={AuthenticationCallback} />
     </Switch>
   </BrowserRouter>
 );
