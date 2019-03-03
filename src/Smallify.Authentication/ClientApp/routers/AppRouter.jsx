2176682f9f5b3301d2aef4ef82eca15921a640ee
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-03-03 19:09:15 +0000

    Updated AppRouter to display AuthenticationDisplay as root/home

--- src/Smallify.Authentication/ClientApp/routers/AppRouter.jsx
+++ src/Smallify.Authentication/ClientApp/routers/AppRouter.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import AuthenticationDisplay from '../components/authentication/AuthenticationDisplay';
+
 const AppRouter = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" />
+      <Route exact path="/" component={AuthenticationDisplay} />
     </Switch>
   </BrowserRouter>
 );
