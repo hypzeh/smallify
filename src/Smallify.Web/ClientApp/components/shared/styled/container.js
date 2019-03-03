a81619544a2a8040f58d9661dc3ba3cdc52c9a4d
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-03-03 21:48:35 +0000

    Removed Smallify.Web project from master branch (#18)
    * Removed Smallify.Web project from master branch
    
    * Updated and renamed azure-pipelines yml
    
    * Updated README

--- src/Smallify.Web/ClientApp/components/shared/styled/container.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const Container = styled.div`
-  padding: 70px 0;
-  text-align: center;
-`;
-
-export default Container;
--- src/Smallify.Web/ClientApp/lib/authentication.js
+++
@@ -1,9 +0,0 @@
-import Settings from "../../appsettings.json";
-
-const Authentication = {
-  ClientID: Settings.Authentication.ClientID,
-  RedirectURI: Settings.Authentication.RedirectURI,
-  Scopes: Settings.Authentication.Scopes
-};
-
-export { Authentication };
