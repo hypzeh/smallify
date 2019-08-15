99f794fc2d4d0bf12ced9afb43637728b1d650f4
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-08-15 22:39:13 +0100

    Removed project

--- src/Smallify.Module.Settings/Constants/RegionNames.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Smallify.Module.Settings.Constants
-{
-	public static class RegionNames
-	{
-		public const string SETTINGS_BUTTON_REGION = nameof(SETTINGS_BUTTON_REGION);
-
-		public const string SETTINGS_SECTION_REGION = nameof(SETTINGS_SECTION_REGION);
-	}
-}
