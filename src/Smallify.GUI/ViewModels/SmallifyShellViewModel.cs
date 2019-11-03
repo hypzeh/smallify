7d3b1b3e40125e6b9698ff5e00bed820cc4f68d8
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-11-03 21:56:55 +0000

    Added testing text to check app settings post CI build

--- src/Smallify.GUI/ViewModels/SmallifyShellViewModel.cs
+++ src/Smallify.GUI/ViewModels/SmallifyShellViewModel.cs
@@ -6,6 +6,7 @@ namespace Smallify.GUI.ViewModels
     internal class SmallifyShellViewModel : BindableBase
     {
         private string _title;
+        private string _testingText;
 
         public string Title
         {
@@ -13,9 +14,16 @@ namespace Smallify.GUI.ViewModels
             private set => SetProperty(ref _title, value);
         }
 
+        public string TestingText
+        {
+            get => _testingText;
+            private set => SetProperty(ref _testingText, value);
+        }
+
         public SmallifyShellViewModel()
         {
-            _title = $"Smallify - {Resources.APP_CENTER_CLIENT_ID}";
+            _title = "Smallify";
+            _testingText = $"app_center_id:{Resources.APP_CENTER_CLIENT_ID}\nspotify_id:{Resources.SPOTIFY_CLIENT_ID}\nspotify_secret:{Resources.SPOTIFY_CLIENT_SECRET}";
         }
     }
 }
