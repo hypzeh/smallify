f7ca83a24eaa420e415cdfedf99c6dfcb2452839
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-11-05 21:56:16 +0000

    Created settings button view

---
+++ src/Smallify.Module.Settings/Views/SettingsButtonView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Smallify.Module.Settings.Views
+{
+    internal partial class SettingsButtonView : UserControl
+    {
+        public SettingsButtonView()
+        {
+            InitializeComponent();
+        }
+    }
+}
