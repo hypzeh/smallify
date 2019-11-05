38252576ac441aa9aa32481eb3ca38493887bee6
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-11-05 21:38:03 +0000

    Created smallify settings module project

---
+++ src/Smallify.Module.Settings/Module.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using System;
+
+namespace Smallify.Module.Settings
+{
+    public class Module : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
