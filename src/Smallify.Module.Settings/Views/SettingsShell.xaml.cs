438186aa3b04a6bcac4d7697aafe618c91979800
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-11-05 21:40:57 +0000

    Created settings shell

---
+++ src/Smallify.Module.Settings/Views/SettingsShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Smallify.Module.Settings.Views
+{
+    /// <summary>
+    /// Interaction logic for SettingsShell.xaml
+    /// </summary>
+    internal partial class SettingsShell : Window
+    {
+        public SettingsShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
