b03eed7f9767f382d3b44d4a7fe6966099f5bb69
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-11-06 23:38:26 +0000

    Created settings shell view model

---
+++ src/Smallify.Module.Settings/ViewModels/SettingsShellViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using Smallify.Module.Settings.Configuration;
+using System.Windows.Input;
+
+namespace Smallify.Module.Settings.ViewModels
+{
+    internal class SettingsShellViewModel : BindableBase
+    {
+        public IRegionManager RegionManager { get; }
+
+        public ICommand SetSectionCommand { get; }
+
+        public SettingsShellViewModel(IRegionManager regionManager)
+        {
+            RegionManager = regionManager;
+            SetSectionCommand = new DelegateCommand<string>(SetSectionCommand_Execute);
+        }
+
+        private void SetSectionCommand_Execute(string section)
+        {
+            RegionManager.RequestNavigate(RegionNames.Section, section);
+        }
+    }
+}
