1a45e852007551c97164fdd345ab066bc8fb05fb
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-11-06 23:39:47 +0000

    Created section names config

---
+++ src/Smallify.Module.Settings/Configuration/SectionNames.cs
@@ -0,0 +1,9 @@
+﻿using Smallify.Module.Settings.Views;
+
+namespace Smallify.Module.Settings.Configuration
+{
+    public static class SectionNames
+    {
+        public const string General = nameof(GeneralSectionView);
+    }
+}
