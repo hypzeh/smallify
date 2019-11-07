757bfcbb30acf7674cd976e74d718542c8729eee
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-11-07 21:31:33 +0000

    Created authentication section view

---
+++ src/Smallify.Module.Settings/Views/AuthenticationSectionView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Smallify.Module.Settings.Views
+{
+    internal partial class AuthenticationSectionView : UserControl
+    {
+        public AuthenticationSectionView()
+        {
+            InitializeComponent();
+        }
+    }
+}
