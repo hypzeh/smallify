930d9c7b8a733eb0d44c1173a7322f55e3b5e93d
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-11-08 21:00:17 +0000

    Created simple general settings

---
+++ src/Smallify.Core/Configuration/GeneralSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace Smallify.Core.Configuration
+{
+    public class GeneralSettings
+    {
+        public bool IsAlwaysOnTop { get; private set; }
+
+        public void SetIsAlwaysOnTop(bool isAlwaysOnTop)
+        {
+            IsAlwaysOnTop = isAlwaysOnTop;
+        }
+    }
+}
