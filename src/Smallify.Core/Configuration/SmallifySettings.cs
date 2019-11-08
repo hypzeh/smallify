6f1c4e3794d5437099c91e73a840db852468be88
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-11-08 21:00:25 +0000

    Created smallify settings

---
+++ src/Smallify.Core/Configuration/SmallifySettings.cs
@@ -0,0 +1,12 @@
+﻿namespace Smallify.Core.Configuration
+{
+    public class SmallifySettings
+    {
+        public GeneralSettings General { get; }
+
+        public SmallifySettings()
+        {
+            General = new GeneralSettings();
+        }
+    }
+}
