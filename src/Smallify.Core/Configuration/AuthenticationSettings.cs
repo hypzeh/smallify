e41bee9eabbaf56ffa6c64f4fa68d55648b1545a
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-11-09 14:23:02 +0000

    Created authentication settings

---
+++ src/Smallify.Core/Configuration/AuthenticationSettings.cs
@@ -0,0 +1,21 @@
+﻿namespace Smallify.Core.Configuration
+{
+    public class AuthenticationSettings
+    {
+        public string ClientID { get; }
+        public string ClientSecret { get; }
+        public string AccessToken { get; private set; }
+
+        public AuthenticationSettings(string clientId, string clientSecret)
+        {
+            ClientID = clientId;
+            ClientSecret = clientSecret;
+            AccessToken = string.Empty;
+        }
+
+        public void SetAccessToken(string accessToken)
+        {
+            AccessToken = accessToken;
+        }
+    }
+}
