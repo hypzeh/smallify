a06589f40e289b39a66a239e0078224c2ae8ca30
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-11-09 14:25:05 +0000

    Removed testing text from smallify shell

--- src/Smallify.GUI/ViewModels/SmallifyShellViewModel.cs
+++ src/Smallify.GUI/ViewModels/SmallifyShellViewModel.cs
@@ -1,12 +1,10 @@
 ﻿using Prism.Mvvm;
-using Smallify.GUI.Properties;
 
 namespace Smallify.GUI.ViewModels
 {
     internal class SmallifyShellViewModel : BindableBase
     {
         private string _title;
-        private string _testingText;
 
         public string Title
         {
@@ -14,16 +12,10 @@ namespace Smallify.GUI.ViewModels
             private set => SetProperty(ref _title, value);
         }
 
-        public string TestingText
-        {
-            get => _testingText;
-            private set => SetProperty(ref _testingText, value);
-        }
 
         public SmallifyShellViewModel()
         {
             _title = "Smallify";
-            _testingText = $"app_center_id: {Resources.APP_CENTER_CLIENT_ID}\nspotify_id: {Resources.SPOTIFY_CLIENT_ID}\nspotify_secret: {Resources.SPOTIFY_CLIENT_SECRET}";
         }
     }
 }
