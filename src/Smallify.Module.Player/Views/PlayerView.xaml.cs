8c31abf3a29268940ea74b7b86753287ab316fa1
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-11-09 21:59:39 +0000

    Created player view

---
+++ src/Smallify.Module.Player/Views/PlayerView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Smallify.Module.Player.Views
+{
+    internal partial class PlayerView : UserControl
+    {
+        public PlayerView()
+        {
+            InitializeComponent();
+        }
+    }
+}
