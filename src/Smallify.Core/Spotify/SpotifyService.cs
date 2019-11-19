f1a8cfed50726c5b8159eae9114d3ecb8b89e277
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-11-19 22:32:19 +0000

    Created basic spotify service with authentication

---
+++ src/Smallify.Core/Spotify/SpotifyService.cs
@@ -0,0 +1,94 @@
+﻿using Smallify.Core.Configuration;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using SpotifyAPI.Web.Enums;
+using SpotifyAPI.Web.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Smallify.Core.Spotify
+{
+    public class SpotifyService
+    {
+        private readonly AuthenticationSettings _settings;
+        private readonly AuthorizationCodeAuth _authentication;
+        private readonly SpotifyWebAPI _api;
+
+        public SpotifyService(AuthenticationSettings settings)
+        {
+            _settings = settings;
+            _authentication = new AuthorizationCodeAuth(
+                clientId: _settings.ClientID,
+                secretId: _settings.ClientSecret,
+                redirectUri: _settings.RedirectURI,
+                serverUri: _settings.RedirectURI,
+                scope: Scope.UserReadPrivate
+                    | Scope.UserReadCurrentlyPlaying
+                    | Scope.UserReadPlaybackState
+                    | Scope.UserModifyPlaybackState);
+            _api = new SpotifyWebAPI
+            {
+                AccessToken = _settings.Token.AccessToken,
+                TokenType = "Bearer",
+                UseAuth = true,
+            };
+        }
+
+        public void OpenBrowser()
+        {
+            _authentication.OpenBrowser();
+        }
+
+        public async Task<PrivateProfile> GetCurrentUserAsync()
+        {
+            var response = await _api.GetPrivateProfileAsync();
+            if (response.HasError())
+            {
+                await RefreshTokenAsync();
+            }
+
+            return response;
+        }
+
+        private async Task RefreshTokenAsync()
+        {
+            if (string.IsNullOrEmpty(_settings.Token.RefreshToken))
+            {
+                await ExchangeCodeAsync();
+                return;
+            }
+
+            AssignToken(
+                await _authentication.RefreshToken(_settings.Token.RefreshToken),
+                () => _settings.SetToken(new AuthenticationToken(string.Empty, string.Empty, 0, DateTimeOffset.UtcNow)));
+        }
+
+        private async Task ExchangeCodeAsync()
+        {
+            if (string.IsNullOrEmpty(_settings.AuthorisationCode))
+            {
+                return;
+            }
+
+            AssignToken(
+                await _authentication.ExchangeCode(_settings.AuthorisationCode),
+                () => _settings.SetAuthorisationCode(string.Empty));
+        }
+
+        private void AssignToken(Token token, Action onError)
+        {
+            if (token.HasError())
+            {
+                onError();
+                return;
+            }
+
+            _api.AccessToken = token.AccessToken;
+            _settings.SetToken(new AuthenticationToken(
+                accessToken: token.AccessToken,
+                refreshToken: token.RefreshToken,
+                expiryLength: Convert.ToInt32(token.ExpiresIn),
+                timestamp: token.CreateDate));
+        }
+    }
+}
