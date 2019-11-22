7396dcc0a5d3b54d522325db9e856c7f6fd49ba2
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-11-22 19:57:32 +0000

    Added clean outputs script to solution

--- scripts/Clean-Outputs.ps1
+++ scripts/Clean-Outputs.ps1
@@ -2,4 +2,3 @@ Get-ChildItem ..\src -include bin,obj -Recurse | foreach ($_) {
   Write-Host "DELETING:" $_.fullname
   Remove-Item $_.fullname -Force -Recurse
 }
-Read-Host -Prompt "Press Enter to exit"
