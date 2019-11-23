859ed8c553bee14d6b469374121a30eb94843b1a
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-11-23 16:12:52 +0000

    Updated window styles and applied templates to shell and notifications

--- src/Smallify.Module.Notifications/ViewModels/NotificationsShellViewModel.cs
+++ src/Smallify.Module.Notifications/ViewModels/NotificationsShellViewModel.cs
@@ -1,19 +1,29 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 using Prism.Regions;
 using Smallify.Module.Notifications.Configuration;
 using Smallify.Module.Notifications.Views;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Smallify.Module.Notifications.ViewModels
 {
     internal class NotificationsShellViewModel : BindableBase
     {
         public IRegionManager RegionManager { get; }
+        public ICommand ExitCommand { get; }
 
         public NotificationsShellViewModel(IRegionManager regionManager)
         {
             RegionManager = regionManager.CreateRegionManager();
+            ExitCommand = new DelegateCommand<Window>(ExitCommand_Execute);
 
             RegionManager.RegisterViewWithRegion(RegionNames.Content, typeof(NotificationsContentView));
+        }
+
+        public void ExitCommand_Execute(Window window)
+        {
+            window.Close();
         }
     }
 }
