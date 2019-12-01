dca1bf402530f429033e861fcc7dd0a45caff30f
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-12-01 21:32:03 +0000

    Updated site

---
+++ precache-manifest.7a6682aa6c4e107b924181760d10b9d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d61ec915fc04c30aa953",
+    "url": "/0.css"
+  },
+  {
+    "url": "/commons.d61ec915fc04c30aa953.chunk.js"
+  },
+  {
+    "url": "/commons.d61ec915fc04c30aa953.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de889b6aa8c1f3c7f6e299f74e699be9",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "/fonts/open-sans-v16-latin-regular.woff"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "/fonts/open-sans-v16-latin-regular.woff2"
+  },
+  {
+    "revision": "e8af3e5b40e7e696b49d5db41b5a8cdb",
+    "url": "/index.html"
+  },
+  {
+    "url": "/main.5d203f733f116b8fdb68.chunk.js"
+  },
+  {
+    "url": "/runtime~main.5982b55eedadb25404fc.js"
+  }
+]);
